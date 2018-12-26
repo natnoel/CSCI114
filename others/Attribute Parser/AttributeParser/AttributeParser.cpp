301ee935791bc19d682cb669e2fe00f4a98363c1
natnoel <natnoel@live.com>
2018-12-26 16:27:57 +0800

    removed use of eraseSubStr method
    removed use of erasing all occurrences

--- others/Attribute Parser/AttributeParser/AttributeParser.cpp
+++ others/Attribute Parser/AttributeParser/AttributeParser.cpp
@@ -30,10 +30,11 @@ int main() {
 
 		if (name[1] != '/') {	//Opening tag
 			//Remove the opening and closing triangle brackets
-			name.erase(std::remove(name.begin(), name.end(), '<'),
+			/*name.erase(std::remove(name.begin(), name.end(), '<'),
 				name.end());
 			name.erase(std::remove(name.begin(), name.end(), '>'),
-				name.end());
+				name.end());*/
+			name.erase(0, 1);
 
 			if (tagName.empty()) {
 				//Root tag
@@ -60,34 +61,43 @@ int main() {
 					tagName.append("~").append(attr);
 					cin >> eq >> rawVal;
 					val = rawVal;
-					val.erase(std::remove(val.begin(), val.end(), '"'),
+					/*val.erase(std::remove(val.begin(), val.end(), '"'),
 						val.end());
 					val.erase(std::remove(val.begin(), val.end(), '>'),
-						val.end());
+						val.end());*/
+					val.erase(0, 1);
+					while (val.back() == '>' || val.back() == '"')
+						val.pop_back();
 
 					//cout << "Added attr: " << tagName << "=" << val << endl;
 					tags.insert(make_pair(tagName, val));
-					eraseSubStr(tagName, "~" + attr);
+					//eraseSubStr(tagName, "~" + attr);
+					tagName.erase(tagName.end() - (attr.length() + 1), tagName.end());
+					//cout << "Tag name is back to: " << tagName << endl;
 				}
 				
 			} while (attr.compare(">") != 0 && rawVal.back() != '>');
 		}
 		else {	//Closing tag
 			//Remove the opening and closing triangle brackets and /
-			name.erase(std::remove(name.begin(), name.end(), '<'),
+			/*name.erase(std::remove(name.begin(), name.end(), '<'),
 				name.end());
 			name.erase(std::remove(name.begin(), name.end(), '>'),
 				name.end());
 			name.erase(std::remove(name.begin(), name.end(), '/'),
-				name.end());
+				name.end());*/
+			name.erase(0, 2);
+			name.pop_back();
 
 			if (tagName.compare(name) == 0) {
 				tagName = "";
 			}
 			else {
 				//name.insert(name.begin(), '.');
-				name.insert(0, ".");
-				eraseSubStr(tagName, name);
+				//name.insert(0, ".");
+				//eraseSubStr(tagName, name);
+				tagName.erase(tagName.end() - (name.length() + 1), tagName.end());
+				//cout << "Tag name is back to: " << tagName << endl;
 			}
 		}
 	}
