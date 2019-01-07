575410717f20402e2487d33bc92cd793d18eef89
natnoel <natnoel@live.com>
2019-01-07 15:42:23 +0800

    fixed something

--- others/python/Nested List/nested_list.py
+++ others/python/Nested List/nested_list.py
@@ -1,9 +1,9 @@
 lowest = sec_lowest = 999
 stud_grades = []
 lowest_stud = sec_lowest_stud = []
-for _ in range(int(input())):
-    name = input()
-    score = float(input())
+for _ in range(int(raw_input())):
+    name = raw_input()
+    score = float(raw_input())
     stud_grades.append([name, score])
 
     if score < lowest:  # new lowest score
@@ -13,15 +13,20 @@ for _ in range(int(input())):
             sec_lowest = lowest
         sec_lowest_stud = lowest_stud       # list of lowest studs is now list of 2nd lowest stud
         lowest_stud = []                    # new lowest stud list
-        lowest_stud.append([name, score])   # add the new entry
-        print('lowest is now', lowest, "second is", sec_lowest)
+        lowest_stud.append(name)   # add the new entry
+        #print('lowest is now', lowest, "second is", sec_lowest)
     elif score < sec_lowest:    # new second lowest (but still less than lowest)
         sec_lowest = score                      # update 2nd lowest score
         sec_lowest_stud = []                    # discard prev 2nd lowest stud list
-        sec_lowest_stud.append([name, score])   # add new entry
+        sec_lowest_stud.append(name)   # add new entry
         print("second is now", sec_lowest, [name,score])
+    elif score == lowest:
+        lowest_stud.append(name)
+        #print("added lowest another", [name, score])
     elif score == sec_lowest:   # another sec lowest student
-        sec_lowest_stud.append([name, score])
-        print("added another", [name, score])
+        sec_lowest_stud.append(name)
+        #print("added 2nd lowest another", [name, score])
 
-print(sec_lowest_stud)
+sec_lowest_stud.sort()
+for name in sec_lowest_stud:
+    print(name)
