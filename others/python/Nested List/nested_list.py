f63984e3e4eed76c5247e6b0524b2b05caa0d120
natnoel <natnoel@live.com>
2019-01-07 16:57:43 +0800

    Doing it all in one loop

--- others/python/Nested List/nested_list.py
+++ others/python/Nested List/nested_list.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
-    stud_list = []
+    stud_list = lowest_studs = sec_lowest_studs =[]
     lowest = sec_lowest = 999
     for _ in range(int(input())):
         name = input()
@@ -9,16 +9,23 @@ if __name__ == '__main__':
         if score < lowest:
             sec_lowest = lowest
             lowest = score
+            sec_lowest_studs = lowest_studs
+            lowest_studs = [name]
         elif score < sec_lowest and score != lowest:
             sec_lowest = score
-        #print('lowest is:', lowest, 'second is:', sec_lowest)
+            sec_lowest_studs = [name]
+        elif score == lowest:
+            lowest_studs.append(name)
+        elif score == sec_lowest:
+            sec_lowest_studs.append(name)
+        print('lowest is:', lowest, 'second is:', sec_lowest)
 
     #print('lowest is:', lowest, 'second is:', sec_lowest)
 
-    sec_lowest_studs = []
-    for stud in stud_list:
-        if (sec_lowest == stud[1]):
-            sec_lowest_studs.append(stud[0])
+    #sec_lowest_studs = []
+    #for stud in stud_list:
+    #    if (sec_lowest == stud[1]):
+    #        sec_lowest_studs.append(stud[0])
 
     sec_lowest_studs.sort()
     for name in sec_lowest_studs:
